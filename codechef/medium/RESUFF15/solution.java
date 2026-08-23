03a197321ae684934e1e30e895d2ff0d1615b933
Charan santosh <25Q91A05AU@mrce.in>
2026-08-23 08:52:33 +0530

    ✅ RESUFF15 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-23

---
+++ codechef/medium/RESUFF15/solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; 
+
+class Codechef {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+        double weight = scanner.nextDouble(); 
+        scanner.nextLine(); 
+        String deliverySpeed = scanner.nextLine(); 
+        if(weight <= 5.0){
+            if(deliverySpeed.equals("Express")){
+                System.out.println("Shipping charge: $15");
+            }else{
+                System.out.println("Shipping charge: $10");
+            }
+        }else{
+            System.out.println("Shipping not available for packages above 5 kg.");
+        }
+        scanner.close(); 
+    }
+}
