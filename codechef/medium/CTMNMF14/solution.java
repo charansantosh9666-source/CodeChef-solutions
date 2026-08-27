0cc10d8708bede45b0cc6cdd6f0751aecfb31bc2
Charan santosh <25Q91A05AU@mrce.in>
2026-08-27 06:25:16 +0530

    ✅ CTMNMF14 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-27

---
+++ codechef/medium/CTMNMF14/solution.java
@@ -0,0 +1,10 @@
+class Codechef {
+    public static void main(String[] args) {
+        double fuel = 7.0; 
+        while ( fuel>1) {
+            System.out.println( "Fuel remaining: "+fuel+" liters." ); 
+            fuel--;
+        }
+        System.out.println("Warning! Only 1.0 liter of fuel left. Refuel soon!");  
+    }
+}
