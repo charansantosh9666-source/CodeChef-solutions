c0a863d510bea4abafe3051fe4291b31ccd5c868
Charan santosh <25Q91A05AU@mrce.in>
2026-08-31 18:40:33 +0530

    ✅ PYTHPROB091 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ codechef/medium/PYTHPROB091/solution.py
@@ -0,0 +1,21 @@
+# Printing Morning Routine
+print("Morning Routine:")
+print("Wake up", end="\n")       # Print the task "Wake up" 
+print("Brush teeth", end="\n")   # Print the task "Brush teeth"
+print("Have breakfast", end="\n")# Print the task "Have breakfast"
+
+print() # Print an empty line to separate the routines
+
+# Printing Afternoon Routine
+print("Afternoon Routine:")
+print("Work/Study", end="\n")    # Print the task "Work/Study"
+print("Lunch", end="\n")         # Print the task "Lunch"
+print("Nap", end="\n")           # Print the task "Nap"
+
+print() # Print an empty line to separate the routines
+
+# Printing Evening Routine
+print("Evening Routine:")
+print("Exercise", end="\n")    
+print("Dinner", end="\n")         
+print("Relax/Leisure time", end="\n")
