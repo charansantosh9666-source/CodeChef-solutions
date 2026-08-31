29bfb0f32ac9a028a6d29eb0dafcbf0c649ab63c
Charan santosh <25Q91A05AU@mrce.in>
2026-08-31 18:36:33 +0530

    ✅ PYTHPROB075 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-31

--- codechef/medium/PYTHPROB075/solution.py
+++ codechef/medium/PYTHPROB075/solution.py
@@ -1,3 +1,2 @@
-# Debug this code to fix the formatting
-print("Success is not final\nFailure is not fatal: It is the courage to continue that counts.")
-print("\t- Winston Churchill")
+# cook your dish here
+print("The programmer exclaimed - \"It\'s amazing how Python handles special characters!\"")
