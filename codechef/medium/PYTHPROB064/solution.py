33ca00593423d3bf045490a3a2dc5b4849df98dc
Charan santosh <25Q91A05AU@mrce.in>
2026-08-31 06:01:28 +0530

    ✅ PYTHPROB064 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ codechef/medium/PYTHPROB064/solution.py
@@ -0,0 +1,5 @@
+# The sep parameter is used to specify the string that separates the items when printed.
+
+# In this example, the items "apple", "banana", "cherry", and "date" are separated by " | ".
+
+print("apple", "banana", "cherry", "date", sep=" | ")
