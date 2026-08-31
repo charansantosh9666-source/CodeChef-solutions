6424fd3322c66d07d25991fec08412860c9c4120
Charan santosh <25Q91A05AU@mrce.in>
2026-08-31 18:41:55 +0530

    ✅ PYTHPROB094 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ codechef/medium/PYTHPROB094/solution.cpp
@@ -0,0 +1,9 @@
+# Printing a formatted string with workout details using an f-string
+Math=90
+Science=85
+English=88
+
+print(f"Math marks are: {Math}, Science marks are: {Science}, English marks are: {English}")
+
+
+
