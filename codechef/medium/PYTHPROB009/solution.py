c03420303e5db8c7a63c23839b14429e3e0ea58b
Charan santosh <25Q91A05AU@mrce.in>
2026-09-01 17:06:29 +0530

    ✅ PYTHPROB009 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-01

---
+++ codechef/medium/PYTHPROB009/solution.py
@@ -0,0 +1,9 @@
+# Prices of the items
+burger_price = 5
+fries_price = 2
+drink_price = 1
+# Calculate total cost here
+total_cost = burger_price+fries_price+drink_price
+
+# Print the total cost
+print("Total cost of the order is:", total_cost)
