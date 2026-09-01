70c2a114bf957ece4ba49516f2bbc1b66e9bb163
Charan santosh <25Q91A05AU@mrce.in>
2026-09-01 17:05:41 +0530

    ✅ PYTHPROB008 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-01

---
+++ codechef/medium/PYTHPROB008/solution.py
@@ -0,0 +1,10 @@
+# Declaring the prices of the items
+apples = 3
+bananas = 2
+oranges = 4
+
+# Performing addition to calculate total cost
+total_cost = apples + bananas + oranges
+
+# Displaying the total cost
+print(total_cost)  # This will output: 9
