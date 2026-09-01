db305c67119244c48e63146b3e775f8df69028ec
Charan santosh <25Q91A05AU@mrce.in>
2026-09-01 17:09:18 +0530

    ✅ PYTHPROB015 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-01

---
+++ codechef/medium/PYTHPROB015/solution.py
@@ -0,0 +1,9 @@
+# Declare the temperatures for City A and City B
+city_a_temp = 75  # Assign with the value of city_a_temp
+city_b_temp =  60 # Assign with the value of city_b_temp
+
+# Calculate the temperature difference
+temperature_difference = city_a_temp - city_b_temp
+
+# Print the result
+print("The temperature difference is:", temperature_difference)
