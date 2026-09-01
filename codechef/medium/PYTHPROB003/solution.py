6fa8289f2883728e4e92d7ccd0a2d80cdb574ac2
Charan santosh <25Q91A05AU@mrce.in>
2026-09-01 17:00:13 +0530

    ✅ PYTHPROB003 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-01

---
+++ codechef/medium/PYTHPROB003/solution.py
@@ -0,0 +1,7 @@
+# Declare a variable to store age
+# Replace 30 with your actual age when using this code
+my_age = 30  
+
+# Print the data type of the variable 'my_age'
+# This will output: <class 'int'> because 'my_age' is an integer
+print(type(my_age))  
