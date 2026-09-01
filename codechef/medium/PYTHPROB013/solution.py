3ec27a0085f3b99fb51930d419a1786318e1d585
Charan santosh <25Q91A05AU@mrce.in>
2026-09-01 17:08:16 +0530

    ✅ PYTHPROB013 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-01

---
+++ codechef/medium/PYTHPROB013/solution.py
@@ -0,0 +1,9 @@
+# Assigning scores to two players
+player_a_score = 28  # Score of Player A
+player_b_score = 15  # Score of Player B
+
+# Calculating the score difference between Player A and Player B
+score_difference = player_a_score - player_b_score  # Subtracting Player B's score from Player A's score
+
+# Printing the score difference to the console
+print(score_difference)  # Output: 13
