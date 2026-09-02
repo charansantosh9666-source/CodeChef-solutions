01f11c37cf5c52267b53f5b155cafb7b58853360
Charan santosh <25Q91A05AU@mrce.in>
2026-09-02 17:27:38 +0530

    ✅ PYTHPROB023 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-02

---
+++ codechef/medium/PYTHPROB023/solution.py
@@ -0,0 +1,11 @@
+# Total number of candies
+candies = 25
+
+# Number of children
+children = 4
+
+# Use Integer Division (//) to calculate how many whole candies each child gets
+candies_per_child = candies // children
+
+# Print the result
+print(candies_per_child)
