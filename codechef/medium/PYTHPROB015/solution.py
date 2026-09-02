150463273c7d1f3b2da7944f44f187b76f7f6c79
Charan santosh <25Q91A05AU@mrce.in>
2026-09-02 17:18:53 +0530

    ✅ PYTHPROB015 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/medium/PYTHPROB015/solution.py
+++ codechef/medium/PYTHPROB015/solution.py
@@ -1,9 +1,6 @@
-# Declare the temperatures for City A and City B
-city_a_temp = 75  # Assign with the value of city_a_temp
-city_b_temp =  60 # Assign with the value of city_b_temp
-
-# Calculate the temperature difference
-temperature_difference = city_a_temp - city_b_temp
-
-# Print the result
-print("The temperature difference is:", temperature_difference)
+# cook your dish here
+b=100
+g=30
+e=20
+print(g+e)
+print(b-(g+e))
