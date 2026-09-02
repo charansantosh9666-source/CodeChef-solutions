8fbeab1cbd16e1a8947b725f376875d3b121b197
Charan santosh <25Q91A05AU@mrce.in>
2026-09-02 18:28:00 +0530

    ✅ LPYAS150 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-02

---
+++ codechef/medium/LPYAS150/solution.py
@@ -0,0 +1,15 @@
+
+
+def main():
+    # Complete the function 
+    a=int(input())
+    while(a>0):
+        b=int(input())
+        if(b%2==0):
+            print("Even")
+        else:
+            print("Odd")
+        a=a-1
+
+if __name__ == "__main__":
+    main()
