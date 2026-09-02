df87c78cba82c215841825f534412e948b3e2af8
Charan santosh <25Q91A05AU@mrce.in>
2026-09-02 17:27:07 +0530

    ✅ PYTHPROB020 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-02

---
+++ codechef/medium/PYTHPROB020/solution.py
@@ -0,0 +1,13 @@
+# Hourly rates for three part-time jobs (in dollars)
+job1_rate = 20
+job2_rate = 15
+job3_rate = 25
+
+# Hours worked in a week for each job
+job1_hours = 10
+job2_hours = 8
+job3_hours = 12
+
+# Update your code below this line
+total=(job1_hours * job1_rate)+(job2_hours * job2_rate)+(job3_hours* job3_rate)
+print(total)
