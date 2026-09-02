7b14c6d7e8b2ccf27b7756d11ccb70e81b4061ba
Charan santosh <25Q91A05AU@mrce.in>
2026-09-02 17:59:20 +0530

    ✅ LPYAS79 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-02

---
+++ codechef/medium/LPYAS79/solution.py
@@ -0,0 +1,8 @@
+# Given dictionary
+student_grades = {"Alice": 85, "Bob": 72, "Charlie": 90, "David": 65, "Eva": 88, "John": 45}
+
+# Complete the code 
+a=input()
+for i in student_grades:
+    if(a==i):
+        print(student_grades[i])
