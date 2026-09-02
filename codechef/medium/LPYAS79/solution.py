375eb1b6fb873b9136264dd9922eaee4765feef8
Charan santosh <25Q91A05AU@mrce.in>
2026-09-02 18:02:47 +0530

    ✅ LPYAS79 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/medium/LPYAS79/solution.py
+++ codechef/medium/LPYAS79/solution.py
@@ -3,6 +3,10 @@ student_grades = {"Alice": 85, "Bob": 72, "Charlie": 90, "David": 65, "Eva": 88,
 
 # Complete the code 
 a=input()
+c=0
 for i in student_grades:
     if(a==i):
         print(student_grades[i])
+        c=c+1
+if(c==0):
+    print("Not Found")
