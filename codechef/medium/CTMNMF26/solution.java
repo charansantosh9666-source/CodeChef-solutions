6f5b06d75a6958732dd30c706a968d7bfad80d78
Charan santosh <25Q91A05AU@mrce.in>
2026-09-03 07:12:49 +0530

    ✅ CTMNMF26 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-03

---
+++ codechef/medium/CTMNMF26/solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Codechef {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Ask the user to input the initial elevation
+        int currentElevation = scanner.nextInt();
+
+        // Loop to decrement elevation by 50 meters until reaching 100 meters or less
+        while(currentElevation>100){
+            System.out.println("Current elevation: "+currentElevation+" meters");
+            currentElevation -=50;
+        }
+     // Display the end of the trail message
+        System.out.println("End of the trail reached at 100 meters or below.");
+    }
+}
