ff9afe8fd32091bf16097ccba8a2e4c85a41e936
Charan santosh <25Q91A05AU@mrce.in>
2026-09-03 07:32:41 +0530

    ✅ PYTHPROB025 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-03

---
+++ codechef/medium/PYTHPROB025/solution.py
@@ -0,0 +1,10 @@
+# Define the total number of cars and cars per row
+total_cars = 23
+cars_per_row = 5
+
+# Calculate the number of full rows using integer division (//)
+n=23//5
+# Calculate the remaining cars using subtraction
+a=n-1
+# Print the results
+print(n,a)
