254cc65235ebec17559c5fe70b042254c6e7c682
Charan santosh <25Q91A05AU@mrce.in>
2026-09-03 07:23:46 +0530

    ✅ CTMNMF33 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-03

--- codechef/medium/CTMNMF33/solution.java
+++ codechef/medium/CTMNMF33/solution.java
@@ -1,11 +1,15 @@
 class Codechef {
     public static void main(String[] args) {
-     
+     a=1;
        // This infinite loop simulates a continuously running server.
         while(true)
         {
              System.out.println("Server is running");
              //Write break; below
+             if(a==4){
+                 break
+             }
+             a++;
         }
     
     }
