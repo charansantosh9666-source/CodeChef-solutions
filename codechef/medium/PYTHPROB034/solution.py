466acc010938d3ec72de52fa2316b6a55d043352
Charan santosh <25Q91A05AU@mrce.in>
2026-09-03 07:36:59 +0530

    ✅ PYTHPROB034 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-03

---
+++ codechef/medium/PYTHPROB034/solution.py
@@ -0,0 +1,9 @@
+# Set the side length of the cube
+side_length = 4 
+
+# Calculate the volume of the cube using the power operator 
+# (side_length raised to the power of 3)
+volume = side_length ** 3 
+
+# Print the calculated volume of the cube
+print(volume)
