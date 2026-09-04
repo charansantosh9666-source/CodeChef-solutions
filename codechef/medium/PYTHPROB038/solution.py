5a064bd4bd4fb08d1a67d3a33be55fc9be820699
Charan santosh <25Q91A05AU@mrce.in>
2026-09-04 08:09:30 +0530

    ✅ PYTHPROB038 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-04

---
+++ codechef/medium/PYTHPROB038/solution.py
@@ -0,0 +1,5 @@
+# This code demonstrates the evaluation of a mathematical expression 
+# with multiple operators, considering the order of operations (precedence).
+
+result = 10 + 2 * 5 - 3 ** 2  # Calculate the result according to operator precedence 
+print(result) 
