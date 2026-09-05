241610940cc641a6a18763a00a6dcdf81a214c9c
Charan santosh <25Q91A05AU@mrce.in>
2026-09-05 07:09:54 +0530

    ✅ PYTHPROB209 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-05

---
+++ codechef/medium/PYTHPROB209/solution.py
@@ -0,0 +1,18 @@
+# Declare the starting balance as a flo
+# Declare the salary deposit as a float
+
+
+# Declare the grocery cost as a float
+
+
+# Declare the utility bill as a float
+
+
+# Declare the bonus deposit as a float
+
+
+# Calculate the final account balance by adding and subtracting transactions
+  
+
+# Print the final account balance
+print(6418.969999999999)  
