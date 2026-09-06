257767fece62b28b35918e90a156838832bba082
Charan santosh <25Q91A05AU@mrce.in>
2026-09-06 07:52:30 +0530

    ✅ PYTHPROB213 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-06

---
+++ codechef/medium/PYTHPROB213/solution.py
@@ -0,0 +1,4 @@
+length = 5.5  # Length of the rectangle
+width = 3.2   # Width of the rectangle
+area = length * width  # Calculating the area by multiplying length and width
+print(area)  # This will output the area of the rectangle
