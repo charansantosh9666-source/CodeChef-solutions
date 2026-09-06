e6b71cec9394022c86afa832c72fd662565725bc
Charan santosh <25Q91A05AU@mrce.in>
2026-09-06 08:33:34 +0530

    ✅ LPYAS129 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-06

--- codechef/medium/LPYAS129/solution.py
+++ codechef/medium/LPYAS129/solution.py
@@ -2,11 +2,6 @@ numbers = list(map(int, input().split()))
 # Update your code below this line
 n=len(numbers)
 for i in range(0,n):
-    a=numbers[i]
-    c=0
-    for j in range(0,n):
-        if(i!=j and a==numbers[j]):
-            c+=1
-    if(c==0):
-        print(a)
+    if(8==numbers[i]):
+        print(i)
         break
