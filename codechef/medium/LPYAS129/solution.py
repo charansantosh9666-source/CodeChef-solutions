bd2849501bb67739827a979fc3308d4777ff319f
Charan santosh <25Q91A05AU@mrce.in>
2026-09-06 08:31:49 +0530

    ✅ LPYAS129 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-06

--- codechef/medium/LPYAS129/solution.py
+++ codechef/medium/LPYAS129/solution.py
@@ -1,3 +1,12 @@
 numbers = list(map(int, input().split()))
 # Update your code below this line
-print(numbers[0])
+n=len(numbers)
+for i in range(0,n):
+    a=numbers[i]
+    c=0
+    for j in range(0,n):
+        if(i!=j and a==numbers[j]):
+            c+=1
+    if(c==0):
+        print(a)
+        break
