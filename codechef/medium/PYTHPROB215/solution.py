cd497e4fc17f1f302dcdd5b1481a86b140b90ef4
Charan santosh <25Q91A05AU@mrce.in>
2026-09-07 06:54:55 +0530

    ✅ PYTHPROB215 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-07

---
+++ codechef/medium/PYTHPROB215/solution.py
@@ -0,0 +1,17 @@
+# Declare the cost per unit as a float
+cost_per_unit = 12.75  
+
+# Declare the number of units needed as an integer
+units_needed = 150  
+
+# Declare the delivery charge as an integer
+delivery_charge = 50  
+
+# Calculate the material cost by multiplying cost per unit with units needed
+
+
+# Add the delivery charge to calculate the total cost
+total_cost=(cost_per_unit* units_needed)+delivery_charge
+
+# Print the total cost
+print(total_cost)
