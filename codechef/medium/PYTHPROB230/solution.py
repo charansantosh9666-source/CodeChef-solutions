4945c4dc8181e22e61fb70ddef58afa5a9d2e85f
Charan santosh <25Q91A05AU@mrce.in>
2026-09-08 07:10:06 +0530

    ✅ PYTHPROB230 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-08

---
+++ codechef/medium/PYTHPROB230/solution.py
@@ -0,0 +1,12 @@
+# Step 1: Declare the distances to the two stars
+distance_star_1 = 40150000000.0  # Distance to the first star in kilometers
+distance_star_2 = 15750000000.0  # Distance to the second star in kilometers
+
+# Step 2: Calculate the difference in distances
+distance_difference = distance_star_1 - distance_star_2
+
+
+# Step 3: Update the blanks in the code below
+print(f"Distance to Star 1: {distance_star_1:.2e} kilometers")
+print(f"Distance to Star 2: {distance_star_2:.2e} kilometers")
+print(f"Difference in distances: {distance_difference:.2e} kilometers")
