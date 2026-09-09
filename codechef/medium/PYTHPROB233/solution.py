70c624bdbde3638ad9541c6db8507e04a9ed6fad
Charan santosh <25Q91A05AU@mrce.in>
2026-09-09 06:42:48 +0530

    ✅ PYTHPROB233 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-09

---
+++ codechef/medium/PYTHPROB233/solution.py
@@ -0,0 +1,15 @@
+# Define the float numbers
+num1 = 0.1
+num2 = 0.2
+
+# Calculate the sum
+result = num1 + num2
+
+# Print the original result
+print("Result without rounding:", result)
+
+# Round the result to one decimal place
+rounded_result = round(result, 1)
+
+# Print the rounded result
+print("Result after rounding:", rounded_result)
