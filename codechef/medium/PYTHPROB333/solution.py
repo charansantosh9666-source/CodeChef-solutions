d0b2bef29bcef6b9ea277288d1d1cb1fc14a8d47
Charan santosh <25Q91A05AU@mrce.in>
2026-09-11 06:44:04 +0530

    ✅ PYTHPROB333 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-11

---
+++ codechef/medium/PYTHPROB333/solution.py
@@ -0,0 +1,8 @@
+# Define a string variable with a message
+my_greeting = "Welcome to Python Programming!"
+
+# Use the len() function to calculate the length of the string
+length_of_greeting = len(my_greeting)
+
+# Print the length of the string
+print(length_of_greeting)  # Output: 30, as the string contains 30 characters, including spaces and punctuation
