b9d32da982fd709f3518125a811294f850746b12
Charan santosh <25Q91A05AU@mrce.in>
2026-09-11 06:43:38 +0530

    ✅ PYTHPROB330 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-11

---
+++ codechef/medium/PYTHPROB330/solution.py
@@ -0,0 +1,12 @@
+# cook your dish here
+a="""Dish: Pancakes
+
+Ingredients:
+- 1 cup all-purpose flour
+- 2 tablespoons sugar
+- 1 teaspoon baking powder
+- 1/2 teaspoon salt
+- 1 cup milk
+- 1 egg
+- 2 tablespoons melted butter"""
+print(a)
