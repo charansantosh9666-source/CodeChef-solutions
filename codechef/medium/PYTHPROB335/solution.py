03d624fd39a16f8e9c0d15d83a7e013f982e86a8
Charan santosh <25Q91A05AU@mrce.in>
2026-09-11 06:46:05 +0530

    ✅ PYTHPROB335 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-11

---
+++ codechef/medium/PYTHPROB335/solution.py
@@ -0,0 +1,9 @@
+# Assign the multi-line comment to a variable as a string
+comment = """
+I love gardening.
+It allows me to connect with nature and grow my own food.
+Every day in the garden is a learning experience.
+"""
+
+# Calculate and print the total number of characters in the comment
+print(len(comment))
