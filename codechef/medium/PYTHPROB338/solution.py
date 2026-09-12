92885f9698680feaae51005d411e7e45f214c0f8
Charan santosh <25Q91A05AU@mrce.in>
2026-09-12 06:37:54 +0530

    ✅ PYTHPROB338 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-12

---
+++ codechef/medium/PYTHPROB338/solution.py
@@ -0,0 +1,16 @@
+# Declare a string variable
+my_string = "Good Morning!"
+
+# Access the first character using positive indexing
+first_character = my_string[0]  # Should give 'G' (the first character of the string)
+
+# Access the fourth character using positive indexing
+fourth_character = my_string[3]  # Should give 'd' (the character at index 3)
+
+# Access the last character using positive indexing
+last_character = my_string[12]  # Should give '!' (the character at index 12)
+
+# Print the extracted characters
+print(first_character)  # Output: G
+print(fourth_character)  # Output: d
+print(last_character)  # Output: !
