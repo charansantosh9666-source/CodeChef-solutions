272f97868082457f91cea3c48c82d01d9650dc71
Charan santosh <25Q91A05AU@mrce.in>
2026-09-12 06:45:06 +0530

    ✅ PYTHPROB345 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-12

---
+++ codechef/medium/PYTHPROB345/solution.py
@@ -0,0 +1,20 @@
+# Define the string
+string = "Learning Python is exciting!"
+
+# Access the last character using negative index -1
+last_char=string[-1]
+
+# Access the second to last character using negative index -2
+second_last_char=string[-2]
+
+# Access the third to last character using negative index -3
+third_last_char=string[-3]
+
+# Access the fourth to last character using negative index -4
+
+fourth_last_char=string[-4]
+# Concatenate the last four characters
+result = fourth_last_char + third_last_char + second_last_char + last_char
+
+# Print the result
+print(result)
