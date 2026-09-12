2f891d80ae5936e24fd02c48b1cf1094ca2ba1dd
Charan santosh <25Q91A05AU@mrce.in>
2026-09-12 06:39:12 +0530

    ✅ PYTHPROB339 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-12

---
+++ codechef/medium/PYTHPROB339/solution.py
@@ -0,0 +1,9 @@
+# Declare a string variable for the weather update
+weather_update = "Today's weather is sunny!"
+
+# Access and print the second character using positive indexing
+print(weather_update[1])
+# Access and print the sixth character using positive indexing
+print(weather_update[5])
+# Access and print the second-to-last character using positive indexing
+print(weather_update[-2])
