f3501cf49bd6039508727e63b0876d2745ffe02a
Charan santosh <25Q91A05AU@mrce.in>
2026-09-13 13:45:30 +0530

    ✅ PYTHPROB354 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-13

---
+++ codechef/medium/PYTHPROB354/solution.py
@@ -0,0 +1,8 @@
+# Assigning a string to the variable 'farewell'
+farewell = "Goodbye! Have a great day ahead!"
+
+# Create a copy of the 'farewell' string using slicing
+farewell_copy = farewell[:]
+
+# Print the copied string
+print(farewell_copy)
