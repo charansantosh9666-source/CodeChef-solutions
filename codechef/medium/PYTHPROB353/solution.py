df2491e36b8f36c09ac2f59eb8bccb5d5e018d62
Charan santosh <25Q91A05AU@mrce.in>
2026-09-13 13:44:42 +0530

    ✅ PYTHPROB353 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-13

---
+++ codechef/medium/PYTHPROB353/solution.py
@@ -0,0 +1,8 @@
+# Assigning a string to the variable 'greeting'
+greeting = "Welcome to Python Programming!"
+
+# Creating a copy of the 'greeting' string using slicing
+greeting_copy = greeting[:]
+
+# Printing the copied string
+print(greeting_copy)
