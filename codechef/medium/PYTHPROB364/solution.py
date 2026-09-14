cafdaac3eea34fbb06939606af58655876a6383a
Charan santosh <25Q91A05AU@mrce.in>
2026-09-14 07:15:03 +0530

    ✅ PYTHPROB364 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-14

---
+++ codechef/medium/PYTHPROB364/solution.py
@@ -0,0 +1,7 @@
+# Assigning the full name to the variable
+full_name = "Jane Smith"
+
+# Slicing the string to get the last name (from index 5 onwards)
+name=full_name[5:]
+# Printing the extracted last name
+print(name)
