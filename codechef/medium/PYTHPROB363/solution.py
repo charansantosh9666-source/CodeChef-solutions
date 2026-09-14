6a35bb54da11bb97f061fd4eb9296a836dfa03c7
Charan santosh <25Q91A05AU@mrce.in>
2026-09-14 07:14:03 +0530

    ✅ PYTHPROB363 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-14

---
+++ codechef/medium/PYTHPROB363/solution.py
@@ -0,0 +1,8 @@
+# Assigning the full name to the variable
+full_name = "John Doe"
+
+# Extracting the first name (first 4 characters of the full name)
+first_name = full_name[0:4]
+
+# Printing the extracted first name
+print(first_name)
