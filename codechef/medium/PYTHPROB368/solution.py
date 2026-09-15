077df53239df113b47b07113cdf6c52cddde9e62
Charan santosh <25Q91A05AU@mrce.in>
2026-09-15 13:54:34 +0530

    ✅ PYTHPROB368 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-15

---
+++ codechef/medium/PYTHPROB368/solution.py
@@ -0,0 +1,8 @@
+# Define the original greeting message
+greeting = "hello, how are you?"
+
+# Convert the greeting to uppercase using the upper() method
+uppercase_greeting = greeting.upper()
+
+# Print the uppercase greetings
+print(uppercase_greeting)
