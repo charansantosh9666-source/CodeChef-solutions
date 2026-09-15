d44342f22874729c98a08490673c9c058da620aa
Charan santosh <25Q91A05AU@mrce.in>
2026-09-15 14:01:21 +0530

    ✅ PYTHPROB373 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-15

---
+++ codechef/medium/PYTHPROB373/solution.py
@@ -0,0 +1,9 @@
+# Define the original greeting message
+greeting = "WELCOME TO PYTHON PROGRAMMING!"
+
+# Convert the greeting to lowercase using the lower() method
+lowercase_greeting = greeting.lower()
+
+# Print the original and lowercase greeting messages
+print(greeting)
+print(lowercase_greeting)
