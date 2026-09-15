9f24d0d8c416ab6741e86c9a1313375fac67b585
Charan santosh <25Q91A05AU@mrce.in>
2026-09-15 13:45:55 +0530

    ✅ PYTHPROB365 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-15

---
+++ codechef/medium/PYTHPROB365/solution.py
@@ -0,0 +1,12 @@
+# Define the string
+text = "PythonProgramming"
+
+# Extract the 5th character using indexing
+
+char_at_index=text[4]
+# Extract the first 6 characters using slicing
+slice_text=text[:6]
+
+# Print the results
+print("Character at index 4:", char_at_index)
+print("First 6 characters:", slice_text)
