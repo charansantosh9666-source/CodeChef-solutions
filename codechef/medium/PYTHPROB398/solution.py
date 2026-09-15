e110fa5fb1b4e5091a4d7caf5f7432c5acc0bdee
Charan santosh <25Q91A05AU@mrce.in>
2026-09-15 14:30:38 +0530

    ✅ PYTHPROB398 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-15

---
+++ codechef/medium/PYTHPROB398/solution.py
@@ -0,0 +1,8 @@
+# Define a string with trailing spaces
+user_message = "Welcome to the Python course!      "
+
+# Remove trailing spaces using rstrip()
+cleaned_message = user_message.rstrip()
+
+# Print the cleaned message without trailing spaces
+print(cleaned_message)
