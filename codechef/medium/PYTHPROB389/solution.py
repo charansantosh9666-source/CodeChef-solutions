31627285db010ec182a27d905d80a2d7c7f3e48e
Charan santosh <25Q91A05AU@mrce.in>
2026-09-15 14:23:14 +0530

    ✅ PYTHPROB389 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-15

--- codechef/medium/PYTHPROB389/solution.py
+++ codechef/medium/PYTHPROB389/solution.py
@@ -1,11 +1,5 @@
 # Step 1: Declare the username with extra spaces
-username = "  TigerKing  "
+username = "     Pancake"
 
-# Step 2: Print the original username (with spaces)
 print(username)
-
-# Step 3: Remove leading and trailing spaces using strip()
-cleaned_username = username.strip()
-
-# Step 4: Print the cleaned username
-print(cleaned_username)
+print(username.strip())
