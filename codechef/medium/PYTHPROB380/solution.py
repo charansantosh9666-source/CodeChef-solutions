c9c4d3a51782d4c20f7c10c03adcbff650648e42
Charan santosh <25Q91A05AU@mrce.in>
2026-09-15 14:11:51 +0530

    ✅ PYTHPROB380 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-15

--- codechef/medium/PYTHPROB380/solution.py
+++ codechef/medium/PYTHPROB380/solution.py
@@ -1,4 +1,16 @@
-# Given animal names in lowercase
-animal_names = "lion, elephant, zebra, giraffe"
+# Given Movie Data
+movie_title = "tHe aVENGerS: eNDgAMe"
+promo_message = "Don't Miss The Epic Finale!"
+disclaimer = "TERMS AND CONDITIONS APPLY."
 
-print(animal_names.title())
+# Formatting the strings
+formatted_title=movie_title.title()
+formatted_promo=promo_message.upper()
+formatted_disclaimer=disclaimer.lower()
+
+
+
+# Display the results
+print(formatted_title)
+print(formatted_promo)
+print(formatted_disclaimer)
