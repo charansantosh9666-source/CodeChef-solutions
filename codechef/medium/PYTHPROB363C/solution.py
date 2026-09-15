aacb5687e421e94ceba92483a8ff44943a742f91
Charan santosh <25Q91A05AU@mrce.in>
2026-09-15 13:47:22 +0530

    ✅ PYTHPROB363C | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-15

---
+++ codechef/medium/PYTHPROB363C/solution.py
@@ -0,0 +1,7 @@
+file_path = "C:/Users/Admin/Documents/project_report.pdf"
+
+# Extract the last 11 characters using negative slicing
+last_part = file_path[-11:]
+
+# Print the result
+print(last_part)
