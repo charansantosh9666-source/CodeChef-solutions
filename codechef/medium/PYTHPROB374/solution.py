463572c1d5a9b9833e380e0bfb5f39cf7edccfb0
Charan santosh <25Q91A05AU@mrce.in>
2026-09-15 14:03:56 +0530

    ✅ PYTHPROB374 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-15

--- codechef/medium/PYTHPROB374/solution.py
+++ codechef/medium/PYTHPROB374/solution.py
@@ -1,9 +1,8 @@
 # Define the original greeting message
-greeting = "WELCOME TO PYTHON PROGRAMMING!"
+greeting = "GoodBye, See You Soon!"
 
 # Convert the greeting to lowercase using the lower() method
-lowercase_greeting = greeting.lower()
+print(greeting)
 
 # Print the original and lowercase greeting messages
-print(greeting)
-print(lowercase_greeting)
+print(greeting.lower())
