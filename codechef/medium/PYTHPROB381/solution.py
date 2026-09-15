5298297d9b4a9998e13f6af91877827a866bafc0
Charan santosh <25Q91A05AU@mrce.in>
2026-09-15 14:11:52 +0530

    ✅ PYTHPROB381 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-15

---
+++ codechef/medium/PYTHPROB381/solution.py
@@ -0,0 +1,16 @@
+# Given Movie Data
+movie_title = "tHe aVENGerS: eNDgAMe"
+promo_message = "Don't Miss The Epic Finale!"
+disclaimer = "TERMS AND CONDITIONS APPLY."
+
+# Formatting the strings
+formatted_title=movie_title.title()
+formatted_promo=promo_message.upper()
+formatted_disclaimer=disclaimer.lower()
+
+
+
+# Display the results
+print(formatted_title)
+print(formatted_promo)
+print(formatted_disclaimer)
