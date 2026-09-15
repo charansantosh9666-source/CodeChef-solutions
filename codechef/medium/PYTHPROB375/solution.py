956558b6327f108ea4db6967afbc47b461d9bed8
Charan santosh <25Q91A05AU@mrce.in>
2026-09-15 14:05:46 +0530

    ✅ PYTHPROB375 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-15

---
+++ codechef/medium/PYTHPROB375/solution.py
@@ -0,0 +1,6 @@
+# Define the original welcome message
+welcome_message = "welcome to the Python programming course!"
+
+welcome_message+=" john"
+lowercase_welcome=welcome_message.lower()
+print(lowercase_welcome)
