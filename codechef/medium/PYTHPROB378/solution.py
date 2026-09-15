9022e81e9ec10e67a240d8d03e8f7a1383d0219d
Charan santosh <25Q91A05AU@mrce.in>
2026-09-15 14:07:00 +0530

    ✅ PYTHPROB378 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-15

---
+++ codechef/medium/PYTHPROB378/solution.py
@@ -0,0 +1,8 @@
+# Assigning the string with city names to the variable city_names
+city_names = "new york, los angeles, chicago, houston"
+
+# Applying the title() method to convert the city names to title case
+formatted_city_names = city_names.title()
+
+# Printing the formatted city names in title case
+print(formatted_city_names)
