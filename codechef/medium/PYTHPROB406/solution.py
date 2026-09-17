28868b7b469c6bf6db8750cb502bd4977b55bce9
Charan santosh <25Q91A05AU@mrce.in>
2026-09-17 06:43:45 +0530

    ✅ PYTHPROB406 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-17

---
+++ codechef/medium/PYTHPROB406/solution.py
@@ -0,0 +1,18 @@
+# Declare first name and last name
+first_name = "john"
+last_name = "doe3"
+com=first_name+" "+last_name
+# Check if names contain only alphabets
+
+first_valid=first_name.isalpha()
+last_valid=last_name.isalpha()
+formatted_name=com.title()
+# Convert names to title case
+
+
+
+
+# Print results
+print(first_valid)
+print(last_valid)
+print(formatted_name)
