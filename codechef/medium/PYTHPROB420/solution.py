0c08899ca79296088ff20db348bc0d4548e378ee
Charan santosh <25Q91A05AU@mrce.in>
2026-09-21 22:02:14 +0530

    ✅ PYTHPROB420 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-21

---
+++ codechef/medium/PYTHPROB420/solution.py
@@ -0,0 +1,7 @@
+# Define a sentence  
+sentence = "The ocean is vast and full of mysteries."
+
+# Use the find() method to get the starting index of the word "ocean"
+index=sentence.find("ocean")
+# Print the index where "ocean" starts in the sentence
+print(index)
