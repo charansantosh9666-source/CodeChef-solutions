0395c8be3ca8503ed1ac5c9215c2e101e3661d67
Charan santosh <25Q91A05AU@mrce.in>
2026-09-21 22:01:09 +0530

    ✅ PYTHPROB418 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-21

---
+++ codechef/medium/PYTHPROB418/solution.py
@@ -0,0 +1,9 @@
+# Define a string
+text = "I love to eat pizza, tacos, and sushi."
+
+# Find the index of the word "tacos" in the string
+# The find() method returns the starting index of the first occurrence of "tacos", or -1 if not found.
+position = text.find("tacos")
+
+# Output the index where "tacos" is found
+print(position)
