08e9cfd782d3223ef48ac26703c833af2d7f911e
Charan santosh <25Q91A05AU@mrce.in>
2026-09-21 22:04:29 +0530

    ✅ PYTHPROB420 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-21

--- codechef/medium/PYTHPROB420/solution.py
+++ codechef/medium/PYTHPROB420/solution.py
@@ -1,7 +1,15 @@
-# Define a sentence  
-sentence = "The ocean is vast and full of mysteries."
+# Declare the product description
+product_description = "Latest Model - X45Pro is now available in stock!"
 
-# Use the find() method to get the starting index of the word "ocean"
-index=sentence.find("ocean")
-# Print the index where "ocean" starts in the sentence
+# Declare the product code
+product_code = "X45Pro"
+
+# Find the index of the product code in the description
+index=product_description.find(product_code)
+
+# Convert the product code to uppercase
+product_code.upper()
+
+# Print the results
 print(index)
+print(product_code)
