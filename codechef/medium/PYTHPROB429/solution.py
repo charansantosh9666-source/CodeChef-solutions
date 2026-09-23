6a7237df2d26e36d5b70b061271accff2a27da99
Charan santosh <25Q91A05AU@mrce.in>
2026-09-23 07:32:55 +0530

    ✅ PYTHPROB429 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/medium/PYTHPROB429/solution.py
@@ -0,0 +1,8 @@
+# Define the string variable
+text = "The cat sat on the mat. My neighbor has a cat too. The cat is very playful."
+
+# Count the occurrences of "cat"
+
+Count=text.count("cat")
+# Print the result
+print(Count)
