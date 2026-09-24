dce1f1bc3ecc571715a5b03d27d70a7a2eefde34
Charan santosh <25Q91A05AU@mrce.in>
2026-09-24 07:12:22 +0530

    ✅ PYTHPROB434 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-24

---
+++ codechef/medium/PYTHPROB434/solution.py
@@ -0,0 +1,8 @@
+# Declare a message with a greeting
+message = "Happy Birthday, Alice! Wishing you a wonderful year ahead."
+
+# Replace the name "Alice" with "Bob" in the message
+new_message = message.replace("Alice", "Bob")
+
+# Print the updated message
+print(new_message)
