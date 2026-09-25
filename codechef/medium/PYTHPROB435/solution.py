4eb87471871e4861673d0ee579161eeafc45721f
Charan santosh <25Q91A05AU@mrce.in>
2026-09-25 07:23:40 +0530

    ✅ PYTHPROB435 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-25

---
+++ codechef/medium/PYTHPROB435/solution.py
@@ -0,0 +1,8 @@
+# Declare the product description
+description = "This mobile has a powerful processor. Mobile gaming is smooth on this mobile."
+
+description=description.lower()
+
+description=description.replace("mobile","smartphone")
+
+print(description)
