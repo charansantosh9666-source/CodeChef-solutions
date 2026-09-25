16aae6e0ce0fa99c2f073c4ceac82da25624000e
Charan santosh <25Q91A05AU@mrce.in>
2026-09-25 07:24:48 +0530

    ✅ PYTHPROB438 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-25

---
+++ codechef/medium/PYTHPROB438/solution.py
@@ -0,0 +1,8 @@
+# Define a string containing a list of ingredients separated by commas
+ingredient_list = "flour,sugar,eggs,milk,butter"
+
+# Split the string into a list using ", " as the delimiter
+ingredients = ingredient_list.split(",")
+
+# Print the resulting list of ingredients
+print(ingredients)
