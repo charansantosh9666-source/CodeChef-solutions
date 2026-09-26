ad59b19bfd4f061ab63b915e5ec3edd3725d527a
Charan santosh <25Q91A05AU@mrce.in>
2026-09-26 07:31:42 +0530

    ✅ PYTHPROB439 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-26

---
+++ codechef/medium/PYTHPROB439/solution.py
@@ -0,0 +1,8 @@
+# Define a string containing a list of fruits separated by semicolons
+fruit_list = "apple;banana;cherry;date;fig"
+
+# Split the string into a list using ";" as the delimiter
+
+fruits=fruit_list.split(";")
+# Print the resulting list of fruits
+print(fruits)
